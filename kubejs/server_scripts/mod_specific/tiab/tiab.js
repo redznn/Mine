891395a0bed29815be7727c220c0ca73cb756aa7
redznn <h908659@gmail.com>
2023-03-04 12:00:31 -0300

    Mods and configs

---
+++ kubejs/server_scripts/mod_specific/tiab/tiab.js
@@ -0,0 +1,10 @@
+onEvent('recipes', event => {
+	event.shaped('1x tiab:timeinabottle', ['DND', 'VEU', 'DAD'], {
+		D: '#allthemodium:unobtainium_ingot',
+		N: '#forge:storage_blocks/netherite',
+		V: '#forge:storage_blocks/vibranium',
+		U: '#forge:storage_blocks/unobtainium',
+		A: '#forge:storage_blocks/allthemodium',
+		E: '#forge:elytra'
+	  }).id('kubejs:tiab/timeinabottle')
+    })
